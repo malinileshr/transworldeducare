1a023a187237ed6a868772278f9e1a535d9fd428
manojsamudre <manojsamudre@gmail.com>
2019-02-03 12:41:17 +0530

    Added favicon for site

--- wp-content/themes/consultingpress-child-theme/header.php
+++ wp-content/themes/consultingpress-child-theme/header.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/transword-fav-icon.jpg" />
         <?php wp_head(); ?>
     </head>
 

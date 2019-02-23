0168259129c28f995ec6011a9ff671d9ce477e96
malinileshr@gmail.com <malinileshr@gmail.com>
2019-02-23 11:58:58 +0530

    SQL added for local setup and other changes are moved to master branch

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'transworldeducare_db');
+define('DB_NAME', 'transworldeducare');
 
 /** MySQL database username */
 define('DB_USER', 'root');

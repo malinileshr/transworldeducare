b7bc10245ede284624fd3a96511bb0ef7cd9969b
malinileshr@gmail.com <malinileshr@gmail.com>
2019-03-04 20:50:13 +0530

    added UI for graduate page

--- wp-content/themes/consultingpress-child-theme/graduates.php
+++ wp-content/themes/consultingpress-child-theme/graduates.php
@@ -17,6 +17,63 @@ $blog_style = rwmb_meta( 'pg_case_studies_options_style' ) ? : Volcanno::return_
 // Page title template
 get_template_part( 'template-parts/page', 'title' );
 ?>
+
+<style type="text/css">
+	.single_member {
+		position: relative;
+		margin-bottom: 15px;	
+		transition:.5s;
+	}
+	.graduateName {
+		color: #303030;
+		font-size: 18px;
+		text-transform: uppercase;
+		font-weight: 500 !important;
+		margin-bottom: 0 !important;
+	}
+	.location{
+		color: #303030;
+		font-size: 13px;
+		font-weight: 500 !important;	
+	}
+	.gradimg{
+		border-radius: 5px;
+		max-height: 262px;
+		overflow: hidden;		
+	    margin-bottom: 10px;
+	}
+	.gradimg img{
+		-webkit-filter: grayscale(100%);
+		filter: grayscale(100%);
+		width:100%;
+		border-radius: 6px;
+		transition:.5s;		
+	}
+	.single_member:hover .gradimg img{
+		-webkit-filter: grayscale(0%);
+		filter: grayscale(0%);		
+			transition:.5s;		
+	}
+	.passyr {
+		color: #fff;
+		font-size: 13px;
+		font-weight: 500 !important;
+		position: absolute;
+		right: 10px;
+		top: 10px;
+		background-color: #ae8b78;
+		z-index: 1;
+		margin-bottom: 0 !important;
+		padding-bottom: 0 !important;
+		padding: 0 15px;
+		border-radius: 3px;
+	}	
+	.single_member:hover .passyr {
+		background-color: #014944;
+	}
+</style>
+
+
 <!-- .page-content start -->
 <div class="page-content all-graduates">
 	<div class="container">
@@ -27,28 +84,25 @@ get_template_part( 'template-parts/page', 'title' );
 			$attachments = get_posts( $args );
 			if ( $attachments ) {
 				foreach ( $attachments as $post ) {
-					echo '<div class="col-md-3 single_member">';
+					echo '<div class="col-md-3 col-sm-3 col-xs-12"><div class="single_member">';	
 					
-					echo '<h3 class="graduateName">'.get_the_title().'</h3>';
-
+					$passingYear = get_post_meta( get_the_ID(), 'passing_year',true );
+					echo '<p class="passyr">'.$passingYear;					
 					$thumbnail = get_the_post_thumbnail_url(get_the_ID(),'full');
 
 					if ( $thumbnail ) {
-						echo 'Image ='.$thumbnail;
+						echo '<div class="gradimg"><img src="'.$thumbnail.'"></div>';
 					} else {
 						$thumbnail = get_stylesheet_directory_uri().'/images/avatar-student.png';
-						echo 'AvTAR ='.$thumbnail;
+						echo '<div class="gradimg"><img src="'.$thumbnail.'"></div>';
 					}
 
 
+					echo '<h3 class="graduateName">'.get_the_title().'</h3>';
+
 					$location = get_post_meta( get_the_ID(), 'branch_location',true );
-					echo '<p>LOCATION ='.$location;
-
-
-					$passingYear = get_post_meta( get_the_ID(), 'passing_year',true );
-					echo '<p>Passing Year ='.$passingYear;
-
-					echo '</div>';
+					echo '<p class="location">'.$location;
+					echo '</div></div>';
 				}
 				wp_reset_postdata();
 			}
